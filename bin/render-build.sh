e66ae760f87186a62612ea3479319c1a818c71fe
hinata <hnoguchi132@outlook.jp>
2025-08-07 10:59:42 +0900

    render-build.sh修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,22 @@
 set -o errexit
 
+# Node.js dependencies installation
+npm install
+
+# Build Tailwind CSS
+npm run build:css
+
+# Build JavaScript
+npm run build
+
+# Ruby dependencies installation
 bundle install
+
+# Precompile assets
 bundle exec rails assets:precompile
+
+# Clean old assets
 bundle exec rails assets:clean
+
+# Run database migrations
 bundle exec rails db:migrate
