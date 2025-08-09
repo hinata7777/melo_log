7ff565a097613ae87ab3cb4df6f251f71b43e054
hinata <hnoguchi132@outlook.jp>
2025-08-09 15:06:45 +0900

    投稿詳細ページの表示変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
-  before_action :ensure_owner!, only: :show
+  # 未ログインでも new/create/show は見られる
+  skip_before_action :require_login, only: %i[new create show]
+
+  before_action :set_user, only: %i[show edit update]
+  before_action :require_login, only: %i[edit update]
+  before_action :ensure_owner!, only: %i[edit update]
 
   def new
     @user = User.new
@@ -8,41 +12,40 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
     if @user.save
-      auto_login(@user)
       redirect_to root_path, notice: 'ユーザー登録が完了しました！'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  # ★ 公開マイページ
   def show
-    @user = current_user
-    @posts = @user.posts.order(created_at: :desc)
+    @posts = @user.posts.includes(:song).order(created_at: :desc)
   end
 
-  def edit
-  @user = current_user
-end
+  def edit; end
 
-def update
-  @user = current_user
-  if @user.update(user_params)
-    redirect_to @user, notice: 'プロフィールを更新しました！'
-  else
-    render :edit, status: :unprocessable_entity
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'プロフィールを更新しました！'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
-end
 
   private
 
-  def user_params
-    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
+  def set_user
+    @user = User.find(params[:id])
   end
 
+  # edit/update は本人のみ
   def ensure_owner!
-    return if logged_in? && params[:id].to_s == current_user.id.to_s
-    redirect_to user_path(current_user)
+    redirect_to root_path, alert: '権限がありません。' unless current_user == @user
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation)
   end
 end
