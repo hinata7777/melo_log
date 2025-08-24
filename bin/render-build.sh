54db75a19736ccba9715479adc8cfa0a5e85628f
hinata <hnoguchi132@outlook.jp>
2025-08-24 16:56:21 +0900

    admin情報削除

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,3 @@ bundle install
 bundle exec rails assets:precompile  # ← ここでTailwind CSSのビルドが必要
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-[ -n "$ADMIN_EMAIL" ] && bundle exec rails runner \
-  "u=User.find_by(email: ENV['ADMIN_EMAIL']) or abort('not found'); u.update!(role: :admin); puts 'admin granted'"
